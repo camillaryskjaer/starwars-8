6e761a803c5b355f214d8c99d6a210ce907384ca
Emil Andersen <EADR@nnit.com>
2020-08-03 18:43:10 +0200

    Initial Commit

---
+++ Starwars/NoRotationComparer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starwars
+{
+    class NameComparer : IEqualityComparer<Planet>
+    {
+        public bool Equals(Planet x, Planet y)
+        {
+            if (string.Equals(x.Name, y.Name, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int GetHashCode(Planet obj)
+        {
+            return obj.Name.GetHashCode();
+        }
+    }
+
+    public class RotationComparer : IEqualityComparer<Planet>
+    {
+        public bool Equals(Planet x, Planet y)
+        {
+            return (x.RotationPeriod == y.RotationPeriod);
+        }
+
+        public int GetHashCode(Planet obj)
+        {
+            return obj.RotationPeriod.GetHashCode();
+        }
+    }
+}
